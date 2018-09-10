3e3f8f3819d9bf7ff2a0a790ef95d1a339916144
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-09-10 23:29:16 +0200

    add sass watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
 
 gulp.task('reload', function() {
     /**
@@ -8,12 +9,23 @@ gulp.task('reload', function() {
     browserSync.reload();
 });
 
-gulp.task('serve', function(){
+gulp.task('serve',['sass'], function(){
     browserSync({
         server: 'src'
     });
 
     gulp.watch('src/*.html', ['reload']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function(){
+    return gulp.src('src/scss/**/*.scss')
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( gulp.dest('src/css') )
+        .pipe( browserSync.stream() );
+    /**
+     * stream -> to wstrzykniecie
+     * */
 });
 
 gulp.task('default', ['serve']);
